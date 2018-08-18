db0326a94c80a70674de7d90f98e161692fd2865
kukaro <justkukaro@naver.com>
2018-08-18 22:34:53 +0900

    분할성공

---
+++ redstop-ktj/src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import App2 from './App2'
+import RedstopHeader from './RedstopHeader'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+new Vue({
+  el: '#app2',
+  router,
+  components: { App2 },
+  template: '<App2/>'
+})
+
+new Vue({
+  el: '#redstop-header',
+  router,
+  components: { RedstopHeader },
+  template: '<RedstopHeader/>'
+})
