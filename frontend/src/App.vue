b1c4a933ac9ed029aa07025c3a7d88763a818ddc
kukaro <justkukaro@naver.com>
2018-08-19 21:37:38 +0900

    header

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <RedstopHeader></RedstopHeader>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import RedstopHeader from './components/RedstopHeader'
+export default {
+  name: 'App',
+  components: {RedstopHeader}
+}
+</script>
+ 
+<style>
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
+</style>
---
+++ frontend/src/components/RedstopHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="redstopHeader">
+    <ul>
+      <li v-for="item in headerList" :key="item">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RedstopHeader',
+  created: function () {
+    this.$http.get('http://localhost:3000/api/header').then((response) => {
+      this.headerList = response.data
+    })
+  },
+  data: function () {
+    return {
+      headerList: {}
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
