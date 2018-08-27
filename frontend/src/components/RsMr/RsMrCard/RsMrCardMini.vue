7b50558bef7fb9c5eef876aa94d26d8e72728a01
kukaro <justkukaro@naver.com>
2018-08-27 09:34:05 +0900

    greed system

---
+++ frontend/src/components/RsMr/RsMrCard/RsMrCardMini.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="test" ref="hi" class="rs-mr-card-mini" :style="{'margin': '20px', 'width': width, 'height': height+'px', 'left': left, 'top': top + 'px', 'background-color': backgroundColor}">
+    <rs-util-text :init-value="'TOTAL APIS'" :init-color="'#5e5e5e'" :init-size="13"/>
+    <rs-util-text :init-value="43" :init-color="'#4da1ff'" :init-size="50"/>
+  </div>
+</template>
+
+<script>
+import RsUtilText from '../../RsUtil/RsUtilText'
+
+export default {
+  props: ['initWidth', 'initHeight', 'initXPos', 'initYPos', 'initBackgroundColor'],
+  name: 'rs-mr-card-mini',
+  created: function () {
+    // console.log(document.getElementById('test').offsetWidth)
+  },
+  mounted: function () {
+    this.height = this.$el.clientWidth / 2.5 * this.initHeight
+    this.top = this.$el.clientWidth / 2.5 * (this.initYPos - 1)
+    console.log(this.top)
+    // console.log(this.height)
+  },
+  components: {RsUtilText},
+  data: function () {
+    return {
+      width: 100 / 6 * this.initWidth + '%',
+      height: 120,
+      left: 100 / 6 * (this.initXPos - 1) + '%',
+      top: 0,
+      backgroundColor: this.initBackgroundColor
+    }
+  }
+}
+</script>
+
+<style scoped>
+.rs-mr-card-mini {
+  z-index: 4;
+  position: absolute;
+  background-color: white;
+}
+</style>
--- frontend/src/components/RsMrBoard.vue
+++ frontend/src/components/RsMrBoard.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="rs-mr-board">
+    <rs-mr-card-mini :init-width="1" :init-height="1" :init-x-pos="1" :init-y-pos="1" :init-background-color="'red'"/>
+    <rs-mr-card-mini :init-width="1" :init-height="1" :init-x-pos="2" :init-y-pos="2" :init-background-color="'green'"/>
+    <rs-mr-card-mini :init-width="2" :init-height="2" :init-x-pos="2" :init-y-pos="2" :init-background-color="'blue'"/>
   </div>
 </template>
 
 <script>
 import RsUtilText from './RsUtil/RsUtilText'
+import RsMrCardMini from './RsMr/RsMrCard/RsMrCardMini'
 export default {
   name: 'rs-mr-board',
-  components: {RsUtilText},
+  components: {RsMrCardMini, RsUtilText},
   data: function () {
     return {
       test: 'total apis'
