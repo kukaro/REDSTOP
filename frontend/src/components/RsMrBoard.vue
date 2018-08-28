5b9d3a2fdb88c12d35f9c63d08e3f5fb28cdd40d
kukaro <justkukaro@naver.com>
2018-08-28 16:12:46 +0900

    add

--- frontend/src/components/RsMrBoard.vue
+++ frontend/src/components/RsMrBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rs-mr-board">
     <rs-mr-card-mini :init-width="1" :init-height="1" :init-x-pos="1" :init-y-pos="1" :init-background-color="'#ffffff'"/>
-    <rs-mr-card-mini :init-width="1" :init-height="1" :init-x-pos="2" :init-y-pos="2" :init-background-color="'green'"/>
+    <rs-mr-card-mini :init-width="1" :init-height="1" :init-x-pos="3" :init-y-pos="2" :init-background-color="'green'"/>
     <rs-mr-card-mini :init-width="3" :init-height="2" :init-x-pos="1" :init-y-pos="3" :init-background-color="'blue'"/>
   </div>
 </template>
