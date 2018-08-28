966b6d28f1962a40d05bbc14315a65dc724e66a9
kukaro <justkukaro@naver.com>
2018-08-29 07:45:27 +0900

    response time

--- frontend/src/components/RsMr/RsMrCard/RsMrCardMiniResponseTime.vue
+++ frontend/src/components/RsMr/RsMrCard/RsMrCardMiniResponseTime.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="test" class="rs-mr-card-mini-response-time rs-mr-card" :style="{'margin': '20px', 'width': width, 'height': height+'px', 'left': left, 'top': top + 'px'}">
     <div class="inner" :style="{'background-color': backgroundColor}">
-      <rs-util-text :init-value="'Response Time'" :init-color="'#000000'" :init-size="15"/> <br>
+      <b><rs-util-text :init-value="'Response Time'" :init-color="'#000000'" :init-size="15"/></b> <br>
       <div class="rs-heatmap-y-label">
           <div class='rs-heatmap-y-label-li' v-for="(value,key) in yLabel" :key="key" :style="{'width': '100%', 'height': 100 / yLabel.length + '%'}">
             <rs-mr-card-mini-response-time-y-label-item :init-value="value + 's'" :init-size="14"/>
           </div>
       </div>
-      <div class="rs-heatmap">
-
-      </div>
+      <rs-mr-card-mini-response-time-heat-map :init-response-time-list="responseTimeList" :init-x-label="xLabel"/>
       <div class="rs-heatmap-x-label">
           <div class='rs-heatmap-x-label-li' v-for="(value,key) in xLabel" :key="key" :style="{'width': 100 / xLabel.length + '%', 'height': '100%'}">
             <rs-mr-card-mini-response-time-x-label-item :initValue="value" :initSize="10"/>
@@ -23,22 +21,30 @@
 import RsUtilText from '../../RsUtil/RsUtilText'
 import RsMrCardMiniResponseTimeXLabelItem from './RsMrCardMiniResponseTimeXLabelItem'
 import RsMrCardMiniResponseTimeYLabelItem from './RsMrCardMiniResponseTimeYLabelItem'
+import RsMrCardMiniResponseTimeItem from './RsMrCardMiniResponseTimeItem'
+import RsMrCardMiniResponseTimeHeatMap from './RsMrCardMiniResponseTimeHeatMap'
 
 export default {
   props: ['initWidth', 'initHeight', 'initXPos', 'initYPos', 'initBackgroundColor'],
   name: 'rs-mr-card-mini-response-time',
   created: function () {
-    console.log('x label count : ' + this.xLabel.length)
+    this.$http.get('http://localhost:3000/api/response-time').then((response) => {
+      this.responseTimeList = response.data.data
+      var count = 0
+      for (var index = 0; index < this.xLabel.length - 1; index++) {
+        while (this.responseTimeList.length !== count && this.xLabel[index] === this.responseTimeList[count].time) {
+          this.responseTimeList[count].time = index + 1
+          count++
+        }
+      }
+      console.log(this.responseTimeList)
+    })
   },
   mounted: function () {
-    // this.height = this.$el.clientWidth / 2.5 * this.initHeight / this.initWidth
-    // TODO 이거 바꿔야함, header 크기를 raw하게 받음
-    // this.top = this.$el.clientWidth / 2.5 * (this.initYPos - 1) / this.initWidth + 70
-    // console.log(this.top)
-    // console.log(this.height)
-    // console.log(this.$refs)
+    // console.log(this.$el.)
   },
-  components: {RsMrCardMiniResponseTimeYLabelItem, RsMrCardMiniResponseTimeXLabelItem, RsUtilText},
+  components: {
+    RsMrCardMiniResponseTimeHeatMap, RsMrCardMiniResponseTimeItem, RsMrCardMiniResponseTimeYLabelItem, RsMrCardMiniResponseTimeXLabelItem, RsUtilText},
   data: function () {
     return {
       width: 100 / 6 * (this.initWidth ? this.initWidth : 1) + '%',
@@ -47,7 +53,8 @@ export default {
       top: 130 * (this.initYPos - 1) + 70,
       backgroundColor: this.initBackgroundColor ? this.initBackgroundColor : '#ffffff',
       xLabel: ['15:30', '15:35', '15:40', '15:45', '15:50', '15:55', '16:00', '16:05', '16:10', '16:15', '16:20', '16:25', '16:30', '16:35', '16:40', '16:45', '16:50', '16:55', '17:00', '17:05', '17:10', '17:15', '17:20', '17:25', '17:30', '17:35', '17:40', '17:45', '17:50'],
-      yLabel: [10, 8, 6, 4, 2, 0]
+      yLabel: [10, 8, 6, 4, 2, 0],
+      responseTimeList: []
     }
   }
 }
@@ -71,17 +78,13 @@ img {
   height: 6px;
   float: right;
 }
-.rs-heatmap{
-  margin-left: 25px;
-  width: calc(100% - 20px);
-  height: calc(100% - 46px);
-  /*background-color: green;*/
-}
 .rs-heatmap-x-label{
+  position: absolute;
+  top: calc(100% - 46px);
   /*background-color: red;*/
   height: 16px;
   margin-left: 25px;
-  width: calc(100% - 20px);
+  width: calc(100% - 70px);
 }
 .rs-heatmap-y-label{
   float:left;
