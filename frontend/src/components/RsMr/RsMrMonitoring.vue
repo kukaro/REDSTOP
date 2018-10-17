8145ac95a5eec679f5a2c36c4e58d6fd8c2b9a42
“안지홍” <hongsam0102@gmail.com>
2018-10-17 13:33:54 +0900

    Add Map

--- frontend/src/components/RsMr/RsMrMonitoring.vue
+++ frontend/src/components/RsMr/RsMrMonitoring.vue
@@ -10,25 +10,25 @@
 </template>
 
 <script>
-  import RsHeaderProject from '../RsHeader/RsHeaderProject'
-  import RsCalendar from './RsMrMonitoring/RsMrMonitoringCalendar'
-  import RsMonitoringItem from './RsMrMonitoring/RsMrMonitoringItem'
+import RsHeaderProject from '../RsHeader/RsHeaderProject'
+import RsCalendar from './RsMrMonitoring/RsMrMonitoringCalendar'
+import RsMonitoringItem from './RsMrMonitoring/RsMrMonitoringItem'
 
-  export default {
-    name: 'rs-mr-monitoring',
-    components: {RsHeaderProject, RsCalendar, RsMonitoringItem},
-    created() {
-      this.$http.get('http://localhost:3000/api/apiList')
-        .then((response) => {
-          this.apiList = response.data
-        })
-    },
-    data: function () {
-      return {
-        apiList: []
-      }
+export default {
+  name: 'rs-mr-monitoring',
+  components: {RsHeaderProject, RsCalendar, RsMonitoringItem},
+  created () {
+    this.$http.get('http://localhost:3000/api/apiList')
+      .then((response) => {
+        this.apiList = response.data
+      })
+  },
+  data: function () {
+    return {
+      apiList: []
     }
   }
+}
 </script>
 
 <style scoped>
